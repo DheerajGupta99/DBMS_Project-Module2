6f836c4761e95bdc8c412ad20de9b3b32d6a30ae
Dheeraj Gupta S D <sdheerajgupta99@gmail.com>
2020-06-29 13:42:07 +0530

    DBMS-Module2

---
+++ src/extHash/allFrames.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package extHash;
+
+/**
+ *
+ * @author VISHNUV
+ */
+public interface allFrames {
+    public static mainFrame mf = new mainFrame();  
+    public static outputFrame of = new outputFrame();
+}
